479069fddc87424696212e0f0fa06d275491a8f3
dooory <78720364+dooory@users.noreply.github.com>
2026-05-15 16:17:19 +1200

    Solve 2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,30 @@
-const contains = function() {
-  
+const contains = function(obj, searchValue) {
+    const objValues = Object.entries(obj);
+    let hasValue = false;
+
+    objValues.forEach(([key, value]) => {
+        if (value === searchValue) {
+            console.log(key, value);
+            
+            hasValue = true;
+
+            return;
+        }
+
+        if (typeof searchValue === "number" && isNaN(searchValue) && isNaN(value)) {
+            hasValue = true;
+
+            return;
+        }
+        
+        if (value !== null && typeof value === "object" && contains(value, searchValue)) {
+            hasValue = true;
+
+            return;
+        }
+    })
+
+    return hasValue;
 };
   
 // Do not edit below this line
