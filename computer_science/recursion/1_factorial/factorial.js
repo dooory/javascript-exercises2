c254f8c95ec8948201a00f9fc8d70773d0a93354
dooory <78720364+dooory@users.noreply.github.com>
2026-05-15 15:14:50 +1200

    Solve 1_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,17 @@
-const factorial = function() {
+const factorial = function(n) {
+    if (n < 0 || !Number.isInteger(n)) {
+        return;
+    }
 
+    if (n === 0) {
+        return 1;
+    }
+
+    if (n <= 2) {
+        return n;
+    }
+
+    return n * factorial(n - 1)
 };
 
 // Do not edit below this line
