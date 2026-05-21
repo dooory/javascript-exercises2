8ad2295e84b182ae8fb53c7d7c595acef3659e33
dooory <78720364+dooory@users.noreply.github.com>
2026-05-22 01:43:27 +1200

    Solve 4_Permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,31 @@
-const permutations = function() {
-  
+const permutations = (array) => {
+  // Arrays with less than 2 elements only have 1 mutation
+  if (array.length <= 1) {
+    return [array];
+  }
+
+  // Arrays with 2 elements only have 2 mutation
+  if (array.length === 2) {
+    return [[...array], array.reverse()];
+  }
+
+  let mutations = [];
+
+  array.forEach((element) => {
+    // Filter out element from array
+    let filteredArray = array.filter(
+      (searchElement) => searchElement !== element,
+    );
+
+    // Get all mutations for array without element
+    // Splice element to the beginning of each mutation
+    permutations(filteredArray).map((mutation) => {
+      mutations.push(mutation.toSpliced(0, 0, element));
+    });
+  });
+
+  return mutations;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
--- computer_science/recursion/4_permutations/permutations.spec.js
+++ computer_science/recursion/4_permutations/permutations.spec.js
@@ -1,3 +1,5 @@
+console.clear();
+
 const permutations = require("./permutations");
 
 describe("permutations", () => {
@@ -5,11 +7,11 @@ describe("permutations", () => {
     expect(permutations([])).toEqual([[]]);
   });
 
-  test.skip("1 possible permutation for a set containing 1 number", () => {
+  test("1 possible permutation for a set containing 1 number", () => {
     expect(permutations([1])).toEqual([[1]]);
   });
 
-  test.skip("2 possible permutations for a set containing 2 numbers", () => {
+  test("2 possible permutations for a set containing 2 numbers", () => {
     expect(permutations([1, 2]).sort()).toEqual(
       [
         [1, 2],
@@ -18,7 +20,7 @@ describe("permutations", () => {
     );
   });
 
-  test.skip("6 possible permutations for a set containing 3 numbers", () => {
+  test("6 possible permutations for a set containing 3 numbers", () => {
     expect(permutations([1, 2, 3]).sort()).toEqual(
       [
         [1, 2, 3],
@@ -31,7 +33,7 @@ describe("permutations", () => {
     );
   });
 
-  test.skip("24 possible permutations for a set containing 4 numbers", () => {
+  test("24 possible permutations for a set containing 4 numbers", () => {
     expect(permutations([1, 2, 3, 4]).sort()).toEqual(
       [
         [1, 2, 3, 4],
